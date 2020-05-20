adb362755ffc33807e0f56b8a3fb65a580845a1d
Platt Thompson <plattthompsonprogramming@gmail.com>
2020-05-20 15:06:20 -0700

    updated gitignore

--- server/server.js
+++ server/server.js
@@ -9,48 +9,10 @@ app.use(cors());
 
 const database = {};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/oneFish', (req, res) => {
 	console.log(req.body);
 	res.json("Got your request! I don't have any files to send back though :(");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.post('/twoFish', (req, res) => {
 	const words = req.body.words;
@@ -59,52 +21,12 @@ app.post('/twoFish', (req, res) => {
 	res.json("I've put your info in the database");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.put('/redFish', (req, res) => {
 	const update = req.body.words;
 	database.data = update;
 	console.log(database);
 	res.json("I've updated your info");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.delete('/blueFish', (req, res) => {
 	const del = req.body.words;
@@ -114,26 +36,6 @@ app.delete('/blueFish', (req, res) => {
 		res.json("I've deleted your info");
 	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`App is running on port ${process.env.PORT}`)
